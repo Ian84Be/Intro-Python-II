ea96690baac11dd825e36117b1a1df6d078d7a03
Ian Belknap <ianbelknap@ians-mbp.mynetworksettings.com>
2024-09-23 20:39:49 -0400

    five years later...

--- src/item.py
+++ src/item.py
@@ -1,17 +1,37 @@
-from textwrap import wrap
-from color import Color
-from gameObj import GameObj
+from color import red
+from object import Object
 
-class Item(GameObj):
-    def __init__(self, name, desc, useRooms={}):
+
+class Item(Object):
+    def __init__(self, name, desc, seen=False):
         super().__init__(name, desc)
-        self.useRooms = useRooms
+        self.seen = seen
 
-    def useItem(self, room):
-        if room not in self.useRooms:
-            return f'You used the {self.name}, {Color.RED}nothing happened{Color.END}'
-        else: 
-            return self.useRooms[room]
 
-    def __str__(self):
-        return f'{Color.RED}{self.name}{Color.END}\n{Color.PURPLE}{self.desc}{Color.END}'
+flashlight_desc = ('Your trusty MagLite. '
+                   'Alloy steel packed with D-cell batteries. '
+                   '1000 lumens of blinding power.')
+item_flashlight = Item('FLASHLIGHT', flashlight_desc)
+
+napkin_desc = ('It is a map, hastily drawn on a used napkin. '
+               'You don\'t recognize the symbols, '
+               'but there are a few letters written in pencil: '
+               f'{red('W N W N E')}')
+item_napkin = Item('NAPKIN', napkin_desc)
+
+item_key = Item(
+    'KEY', 'An unusally large, metal key. Feels warm to the touch.')
+item_toy = Item(
+    'TOY', 'A well loved toy. Bright yellow, dripping with dog slobber.')
+
+game_items = [item_key, item_toy, item_flashlight, item_napkin]
+item_names = [item.name for item in game_items]
+
+
+def get_item_desc(subject):
+    unknown_subject = subject not in item_names
+    item_desc = ''
+    if not unknown_subject:
+        item_index = item_names.index(subject)
+        item_desc = str(game_items[item_index])
+    return item_desc
--- src/player.py
+++ src/player.py
@@ -1,70 +1,29 @@
-from gameObj import GameObj
-from color import Color
+from color import red
+from with_items import ObjectWithItems, WithItems
 
-class Player(GameObj):
-    def __init__(self, name, loc, desc='n/a', holding=[]):
-        super().__init__(name, desc, holding)
+
+class Player(WithItems):
+    def __init__(self, obj: ObjectWithItems, loc):
+        super().__init__(obj)
         self.loc = loc
-    
-    def startGame(self):
-        # DRAMATIC INTRO
-        print('\n')
-        self.crawlText('You awaken suddenly.')
-        self.crawlText(f'Your body is aching and your clothes are stained with mud. You {Color.PURPLE}look{Color.END} around to see a locked iron gate behind you, and a gravel pathway before you. How did you get here? You touch your head and feel a lump, it is wet, and sticky. You can see a {Color.RED}Flashlight{Color.END} on the gravel nearby. It\'s YOUR flashlight.', delay=0.03)
-        self.loc.getItem('flashlight', self)
-        self.crawlText(f'{Color.PURPLE}It is wet with blood. Did someone knock you out with your own flashlight?{Color.END}', 0.02)
-        self.crawlText(f'{Color.PURPLE}You can see your name engraved on the handle: {Color.RED}{self.name.upper()}{Color.END}', 0.02)
-        self.crawlText(self.loc.desc, 0.02)
-
-    def drop(self, thisItem):
-        index = None
-        for i, item in enumerate(self.holding):
-            if item.name.lower() == thisItem.lower():
-                index = i
-        if index != None:
-            thisItem = self.holding.pop(index)
-            self.loc.holding.append(thisItem)
-            self.crawlText(f'You dropped the {Color.RED}{thisItem.name}{Color.END}')
-        else:
-            print(f'You cannot see a {Color.RED}{thisItem}{Color.END}')
-
-    def use(self, thisItem):
-        index = None
-        for i, item in enumerate(self.holding):
-            if item.name.lower() == thisItem.lower():
-                index = i
-        if index != None:
-            thisItem = self.holding[index]
-            thisHappened = thisItem.useItem(room=self.loc.name)
-            if thisItem.name == 'Flashlight':
-                self.loc.isDark=False
-            if thisItem.name == 'Key' and self.loc.name == 'Treasure Chamber':
-                self.crawlText(f'{Color.RED}{thisHappened}{Color.END}')
-                self.quitGame()
-            print('\n')
-            self.crawlText(f'{Color.RED}{thisHappened}{Color.END}')
-        else:
-            print(f'You are not holding {Color.RED}{thisItem}{Color.END}')
-
-    def go(self, there):
-        if there not in ['north','east','west','south','up','down']:
-            print(f'{Color.PURPLE}{there}{Color.RED} is not an option{Color.END}')
-        else:
-            goTo = there[0] + '_to'
-            newRoom = getattr(self.loc, goTo)
-            if newRoom != None:
-                self.loc = newRoom
-                if self.loc.seen == False: 
-                    self.loc.seen = True
-                    print('\n')
-                    self.crawlText(f'{Color.PURPLE}{self.loc.name}{Color.END}',0.03)
-                    self.crawlText(self.loc.desc,0.02)
-                else:
-                    print('\n')
-                    self.crawlText(f'{Color.PURPLE}You have been here before{Color.END}',0.03)
-                    print(self.loc)
-            else:
-                print(f'{Color.RED}You cannot go {Color.PURPLE}{there}{Color.RED} from here{Color.END}')
 
     def __str__(self):
         return f'++ {self.name} is at the {self.loc.name} ++'
+
+    def show_items(self):
+        print(f'You are holding {red(self.show_items_text())}\n')
+
+    def act_get(self, item):
+        print(f'You pick up the {red(item.name)}')
+        if self.holding:
+            self.holding.append(item)
+        else:
+            self.holding = [item]
+
+    def act_drop(self, subject):
+        print(f'You dropped the {red(subject)}')
+        item_names = self.get_item_names()
+        if item_names:
+            index = item_names.index(subject)
+            item = self.holding.pop(index)
+            self.loc.act_drop(item)
